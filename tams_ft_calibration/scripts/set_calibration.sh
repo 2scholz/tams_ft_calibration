d978bbb6f3f51d30490a76ec9dcf506b085d8cca
demo <tams-demo@informatik.uni-hamburg.de>
2018-03-09 16:12:05 +0100

    Adjust set_calibration.sh to new service

--- tams_ft_calibration/scripts/set_calibration.sh
+++ tams_ft_calibration/scripts/set_calibration.sh
@@ -3,5 +3,6 @@
 # Note the curly braces for arguments ${10} and up.
 #
 # rosservice call /sunrise_m3207/set_calibration "{ offsets:[ 1,2,3,4,5,6 ], gains:[ 1,1,1,1,2,3 ] }"
-rosservice call set_calibration "{ offsets:[ $1, $2, $3, $4, $5, $6 ], gains:[ $7, $8, $9, ${10}, ${11}, ${12} ] }"
+#rosservice call set_calibration "{ offsets:[ $1, $2, $3, $4, $5, $6 ], gains:[ $7, $8, $9, ${10}, ${11}, ${12} ] }"
+rosservice call set_calibration "{ force_offsets:[ $1, $2, $3], torque_offsets:[$4, $5, $6 ], force_gains:[ $7, $8, $9], torque_gains:[${10}, ${11}, ${12}] }"
 #
